8f2414c4e7face5c99f9575c702948eb5ec4bee2
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-01-25 19:24:48 +0100

    Add project files.

---
+++ Resize_Window_Example/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resize_Window_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ResizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Width = this.Width == 300 ? 600 : 300;
+            this.Height = this.Height == 300 ? 600 : 300;
+        }
+    }
+}
